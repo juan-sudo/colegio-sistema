a510e0f3827fc9422833d31bb20b72bcab167f37
juan-sudo <53407915+juan-sudo@users.noreply.github.com>
2026-09-01 19:51:03 -0500

    Add pgsql connection to config/database.php for Supabase

--- config/database.php
+++ config/database.php
@@ -24,6 +24,21 @@ return [
             'engine' => null,
         ],
 
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
     ],
 
     'migrations' => 'migrations',
